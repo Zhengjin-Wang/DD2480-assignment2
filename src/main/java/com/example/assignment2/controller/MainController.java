b16322b08e71eaebf89d602e169ac7a883ce0dc3
Lilac09 <1054129023@qq.com>
2023-02-02 18:50:20 +0100

    Build up

--- src/main/java/com/example/assignment2/controller/MainController.java
+++ src/main/java/com/example/assignment2/controller/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String test(){
 
         return "test";
